0af11cbf40ca9feda00dc009b6bc754534b660a9
adrianadls <89851640+adrianadls@users.noreply.github.com>
2022-04-04 11:49:43 -0400

    today's work

--- src/Components/pinText.js
+++ src/Components/pinText.js
@@ -5,8 +5,8 @@ import Map from "./Map.js";
 function pin() {
   return (
     <div className="card-container">
-      <div className="card-title">Island Name</div>
-      <p className="card-text description">Description</p>
+      <div className="card-title">Staten Island</div>
+      <p className="card-text description">Name</p>
       <p className="card-text lat-lon">Lat, Lon</p>
       <Map />
     </div>

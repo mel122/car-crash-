5cc1f0f457fb6a15043978d5b19ca446d097032c
adrianadls <89851640+adrianadls@users.noreply.github.com>
2022-04-27 12:25:44 -0400

    today's work 4/27

--- src/App.js
+++ src/App.js
@@ -9,13 +9,25 @@ class App extends Component {
     this.state = {};
   }
   counters = {
-    manhattan: 0,
-    brooklyn: 0
+  STATEN ISLAND: 0,
+BROOKLYN: 0,
+QUEENS:0,
+MANHATTAN:0,
+BRONX:0
   };
+
+
+  <Rows counters ={data[0]} />
+  {data.map((counters) => {
+    return <Rows counters={counters} />;
+  })}
   //map ()
+
+
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <h1>Staten Island Crash Locations 2022</h1>
         <Map />
       </div>

a51125f63438ec58fe442b693dccdc687cb08527
adrianadls <89851640+adrianadls@users.noreply.github.com>
2022-04-28 12:20:47 -0400

    today's work 4/28

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Map from "./Components/map.js";
+import Map from "./Components/interactiveMap.js";
+//import Navbar from ".Components/navbar.js";
 import "bootstrap";
 import "./App.css";
 
@@ -8,26 +9,14 @@ class App extends Component {
     super(props);
     this.state = {};
   }
-  counters = {
-  STATEN ISLAND: 0,
-BROOKLYN: 0,
-QUEENS:0,
-MANHATTAN:0,
-BRONX:0
-  };
+  // counters = { STATEN ISLAND: 0, BROOKLYN: 0, QUEENS:0, MANHATTAN:0,BRONX:0 };
 
-
-  <Rows counters ={data[0]} />
-  {data.map((counters) => {
-    return <Rows counters={counters} />;
-  })}
+  // <Rows counters ={data[0]} /> {data.map((counters) => {  return <Rows counters={counters} />;})}
   //map ()
-
 
   render() {
     return (
       <div className="App">
-        <Navbar/>
         <h1>Staten Island Crash Locations 2022</h1>
         <Map />
       </div>
---
+++ src/Components/interactiveMap.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
+const mapboxToken =
+  "https://data.cityofnewyork.us/resource/h9gi-nx95.json$==pk.eyJ1IjoiYWRyaWFuYWRsczkiLCJhIjoiY2wyajc5M2t6MDl5cTNqbzhocmZ0cWl0YiJ9.a7tm_F4gzekwWxVRLEFyhg";
+
+class Map extends Component {
+  constructor() {
+    super();
+    this.state = {
+      viewport: {
+        width: "100vw",
+        height: "100vh",
+        latitude: 40.78343,
+        longitude: -73.96625,
+        zoom: 11
+      },
+      currMarker: null,
+      markers: [],
+      selectedMarker: null
+    };
+    this.handleViewportChange = this.handleViewportChange.bind(this);
+    this.handleGeocoderViewportChange = this.handleGeocoderViewportChange.bind(
+      this
+    );
+    this.handleResult = this.handleResult.bind(this);
+    this.addMarker = this.addMarker.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
+  }
+  mapRef = React.createRef();
+
+  handleViewportChange(viewport) {
+    this.setState((prevState) => ({
+      viewport: { ...prevState.viewport, ...viewport }
+    }));
+  }
+  handleGeocoderViewportChange(viewport) {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    });
+  }
+  handleResult(result) {
+    this.setState({
+      currMarker: {
+        name: result.result.place_name,
+        latitude: result.result.center[1],
+        longitude: result.result.center[0]
+      }
+    });
+  }
+  addMarker() {
+    const { currMarker } = this.state;
+    this.setState((prevState) => ({
+      markers: [...prevState.markers, currMarker],
+      currMarker: null
+    }));
+  }
+  handleMarkerClick(marker) {
+    this.setState({
+      selectedMarker: marker
+    });
+  }
+  handleClose = () => {
+    this.setState({
+      selectedMarker: null
+    });
+  };
+  render() {
+    const { viewport, markers, selectedMarker } = this.state;
+    return (
+      <ReactMapGl
+        ref={this.mapRef}
+        {...viewport}
+        onViewportChange={(viewport) => this.setState({ viewport })}
+        mapboxApiAccessToken={mapboxToken}
+        mapStyle="mapbox://styles/mapbox/streets-v10"
+      >
+        <button className="add-btn" onClick={this.addMarker}>
+          Add
+        </button>
+        {markers.map((marker, idx) => {
+          return (
+            <Marker
+              key={idx}
+              latitude={marker.latitude}
+              longitude={marker.longitude}
+              onClick={() => this.handleMarkerClick(marker)}
+            >
+              <img src="pin.png" alt="marker" />
+            </Marker>
+          );
+        })}
+        <Geocoder
+          onSelected={this.handleResult}
+          mapRef={this.mapRef}
+          placeholder="Search here!"
+          onViewportChange={this.handleGeocoderViewportChange}
+          onResult={this.handleResult}
+          mapboxApiAccessToken={mapboxToken}
+          countries="us"
+          position="top-right"
+        />
+        {this.state.selectedMarker && (
+          <Popup
+            mapRef={this.mapRef}
+            latitude={selectedMarker.latitude}
+            longitude={selectedMarker.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={this.handleClose}
+          >
+            <h3>{selectedMarker.name}</h3>
+          </Popup>
+        )}
+      </ReactMapGl>
+    );
+  }
+}
+
+export default Map;

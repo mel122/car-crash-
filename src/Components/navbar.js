35b820a790bcfd7d6b43afc89ec239f1b5f8cb4a
adrianadls <89851640+adrianadls@users.noreply.github.com>
2022-05-30 23:22:21 -0400

    today's final work

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,12 +1,20 @@
 import "bootstrap";
 import React, { Component } from "react";
 
+const onClick = () => {
+  window.scroll({
+    bottom: document.body.scrollHeight,
+    left: 0,
+    behavior: "smooth"
+  });
+};
+
 function Navbar() {
   return (
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">Staten Island Crash Locations</span>
-        <button onClick="onClick" type="button" class="btn btn-outline-info">
+        <button onClick={onClick} type="button" class="btn btn-outline-info">
           Go to Graph
         </button>
       </div>
